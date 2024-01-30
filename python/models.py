70de5924c36e3aedf8006267feb5ff25a93b6f88
Pacheco Melanie <melanie.pacheco@student-cs.fr>
2024-01-30 11:28:36 +0100

    Gurobi model

--- python/models.py
+++ python/models.py
@@ -1,6 +1,7 @@
 import pickle
 from abc import abstractmethod
 from gurobipy import Model, GRB
+import gurobipy as gp
 
 import numpy as np
 
@@ -186,6 +187,8 @@ class TwoClustersMIP(BaseModel):
         self.n_pieces = n_pieces
         self.n_clusters = n_clusters
         self.model = self.instantiate()
+        self.w = None
+        self.g = np.linspace(0, 1, self.n_pieces + 1)
         
 
     def instantiate(self):
@@ -207,39 +210,116 @@ class TwoClustersMIP(BaseModel):
         """
 
         # Define parameters
+        
         n_pairs, n_features = X.shape
+        # Breaking points g
+        g = self.g
+
+        # Define alpha and the breaking points
+        alpha_x = {}
+        alpha_y = {}
+        low_up_idx_x = {}
+        low_up_idx_y = {}
+
+        for p in range(n_pairs):
+            for i in range(n_features):
+                # Find the index of the breaking points
+                up_idx_x = np.searchsorted(g, X[p, i])
+                up_idx_y = np.searchsorted(g, Y[p, i])
+                low_idx_x = up_idx_x - 1
+                low_idx_y = up_idx_y - 1
+                
+                # Store the index of the breaking points
+                low_up_idx_x[p, i] = (low_idx_x, up_idx_x)
+                low_up_idx_y[p, i] = (low_idx_y, up_idx_y)
+                
+                # Compute alpha
+                alpha_x[p, i] = (X[p,i] - g[low_idx_x]) / (g[up_idx_x] - g[low_idx_x])
+                alpha_y[p, i] = (Y[p,i] - g[low_idx_y]) / (g[up_idx_y] - g[low_idx_y])
+
+        # Big M
+        M = 1000
+        # Little delta
+        delta = 0.01
+        delta_2 = 0.001
 
         # Define variables
         # Assuming number of clusters (k), number of pieces (l), 
         # number of pairs (p), and number of criterio (i) that come from your problem definition.
 
-        # Create the w^{k,l}_i: weights of the utility function of each feature i for each cluster k and each piece l
+        # Create the w^{k,l}_i: weights of the utility function of each feature i for each cluster k and each piece l (breakingpoint)
+        w = {}
         for k in range(self.n_clusters):
             for l in range(self.n_pieces + 1):
                 for i in range(n_features):
-                    w[k, l, i] = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name=f"w_{k}_{l}_{i}")
+                    w[k, l, i] = self.model.addVar(vtype=GRB.CONTINUOUS, lb=0, name=f"w_{k}_{l}_{i}")
 
         # Create the σ_pk: marginal error of each pair p for each cluster k
         sigma = {}
         for p in range(n_pairs):
             for k in range(self.n_clusters):
-                sigma[p, k] = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name=f"sigma_{p}_{k}")
+                sigma[p, k] = self.model.addVar(vtype=GRB.CONTINUOUS, lb=0, name=f"sigma_{p}_{k}")
 
         # Create the v_pk: binary variable indicating if pair p is explained by cluster k
         v = {}
         for p in range(n_pairs):
             for k in range(self.n_clusters):
-                v[p, k] = m.addVar(vtype=GRB.BINARY, name=f"v_{p}_{k}")
+                v[p, k] = self.model.addVar(vtype=GRB.BINARY, name=f"v_{p}_{k}")
 
         # Integrate new variables
         self.model.update()
 
         # Constrains
 
+        # Each pair p is valid for at least one cluster k
+        for p in range(n_pairs):
+            self.model.addConstr(gp.quicksum(v[p, k] for k in range(self.n_clusters)) >= 1, name=f"valid_pair_{p}")
 
+        # It is valid when the difference of utility between the two elements of the pair is greater than the marginal error
+        for p in range(n_pairs):
+            for k in range(self.n_clusters):
+                
+                Ux = gp.quicksum(w[k, low_up_idx_x[p, i][0], i] * (1 - alpha_x[p, i]) + w[k, low_up_idx_x[p, i][1], i] * alpha_x[p, i] for i in range(n_features))
+                Uy = gp.quicksum(w[k, low_up_idx_y[p, i][0], i] * (1 - alpha_y[p, i]) + w[k, low_up_idx_y[p, i][1], i] * alpha_y[p, i] for i in range(n_features))
 
+                # Create constraint 1
+                self.model.addConstr(Ux - Uy + sigma[p, k] + delta_2 <= v[p, k] * M, name=f"UTA_1_{p}_{k}")
+
+                # Create constraint 2
+                self.model.addConstr(Ux - Uy + sigma[p, k] >=  delta - (1 - v[p, k]) * M, name=f"UTA_2_{p}_{k}")
+        
+        # Monothonicity of preferences 
+        for i in range(n_features):
+            for k in range(self.n_clusters):
+                for l in range(self.n_pieces):
+                    self.model.addConstr(w[k, l, i] <= w[k, l+1, i], name=f"mono_{i}_{k}_{l}")
+        
+        # Normalization of weights
+        for k in range(self.n_clusters):
+            self.model.addConstr(gp.quicksum(w[k, self.n_pieces, i]  for i in range(n_features)) == 1, name=f"norm_{k}")
+        
+        # First weight is 0
+        for k in range(self.n_clusters):
+            self.model.addConstr(gp.quicksum(w[k, 0, i] for i in range(n_features))== 0, name=f"first_{k}_{i}")
+        
+        # Set objective
+        self.model.setObjective(gp.quicksum(sigma[p, k] for p in range(n_pairs) for k in range(self.n_clusters)), GRB.MINIMIZE)
+
+        # Optimize model
+        self.model.optimize()
+
+        # Print 10 first weights
+        for k in range(self.n_clusters):
+            for l in range(self.n_pieces + 1):
+                for i in range(n_features):
+                    print(f"w_{k}_{l}_{i} = {w[k, l, i].x}")
+
+        # Save variables
+        self.w = w 
+        self.g = g
+        
         # To be completed
-        return
+        return self.model
 
     def predict_utility(self, X):
         """Return Decision Function of the MIP for X. - To be completed.
@@ -256,7 +336,26 @@ class TwoClustersMIP(BaseModel):
         """
         # To be completed
         # Do not forget that this method is called in predict_preference (line 42) and therefor should return well-organized data for it to work.
-        return
+
+        u_1 = np.dot(X, self.weights[0]) # Utility for cluster 1 = X^T.w_1
+        u_2 = np.dot(X, self.weights[1]) # Utility for cluster 2 = X^T.w_2
+        return np.stack([u_1, u_2], axis=1) # Stacking utilities over cluster on axis 1
+
+
+    def save_model(self, path):
+        # Save gurobi model
+        self.model.write(path)
+    
+    def load_model(self, path):
+        # Load gurobi model
+        self.model = gp.read(path)
+        # Save variable w
+        self.w = {}
+        for v in self.model.getVars():
+            if v.varName[0] == 'w':
+                self.w[v.varName] = v.x
+                
+        return self.model
 
 
 class HeuristicModel(BaseModel):
@@ -304,3 +403,5 @@ class HeuristicModel(BaseModel):
         # To be completed
         # Do not forget that this method is called in predict_preference (line 42) and therefor should return well-organized data for it to work.
         return
+    
+    
