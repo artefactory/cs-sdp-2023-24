c9bfbdf882a66a2ba97db5f9c32bf85e60c9e4be
Marouane Maamar <marouane.maamar@student-cs.fr>
2024-02-10 17:44:49 +0100

    Two Clusters MIP model

--- python/models.py
+++ python/models.py
@@ -2,7 +2,7 @@ import pickle
 from abc import abstractmethod
 
 import numpy as np
-
+from gurobipy import Model, GRB, max_, quicksum
 
 class BaseModel(object):
     """
@@ -171,7 +171,7 @@ class TwoClustersMIP(BaseModel):
     You have to encapsulate your code within this class that will be called for evaluation.
     """
 
-    def __init__(self, n_pieces, n_clusters):
+    def __init__(self, n_pieces, n_clusters, epsilon):
         """Initialization of the MIP Variables
 
         Parameters
@@ -183,11 +183,15 @@ class TwoClustersMIP(BaseModel):
         """
         self.seed = 123
         self.model = self.instantiate()
+        self.L = n_pieces
+        self.K = n_clusters
+        self.epsilon = epsilon
 
     def instantiate(self):
         """Instantiation of the MIP Variables - To be completed."""
-        # To be completed
-        return
+        np.random.seed(self.seed)
+        model = Model("TwoClustersMIP")
+        return model
 
     def fit(self, X, Y):
         """Estimation of the parameters - To be completed.
@@ -199,9 +203,136 @@ class TwoClustersMIP(BaseModel):
         Y: np.ndarray
             (n_samples, n_features) features of unchosen elements
         """
+        maxs = np.ones(self.n)
+        mins = np.zeros(self.n)
+        self.n = X.shape[1]
+        self.P = X.shape[0]
 
-        # To be completed
-        return
+        #Last index of the piecewise function
+        def li(x, i):
+            return np.floor(self.L * (x - mins[i]) / (maxs[i] - mins[i]))
+        
+        #Lower bound of the piecewise function
+        def xl(i, l):
+            return mins[i] + l * (maxs[i] - mins[i]) / self.L
+        
+        # Variables of our model
+        # Utilitary functions
+        self.U = {
+            (k, i, l): self.model.addVar(
+                vtype=GRB.CONTINUOUS, lb=0, name="u_{}_{}_{}".format(k, i, l), ub=1)
+                for k in range(self.K)
+                for i in range(self.n)
+                for l in range(self.L+1)
+        }
+        uik_xij = {}
+        for k in range(self.K):
+            for i in range(self.n):
+                for j in range(self.P):
+                    l = li(X[j, i], i)
+                    x = xl(i, l)
+                    x1 = xl(i, l+1)
+                    uik_xij[k, i, j] = self.U[(k, i, l)] + ((X[j, i] - x) / (x1 - x)) * (self.U[(k, i, l+1)] - self.U[(k, i, l)])
+        
+        uik_yij = {}
+        for k in range(self.K):
+            for i in range(self.n):
+                for j in range(self.P):
+                    l = li(Y[j, i], i)
+                    x = xl(i, l)
+                    x1 = xl(i, l+1)
+                    uik_yij[k, i, j] = self.U[(k, i, l)] + ((Y[j, i] - x) / (x1 - x)) * (self.U[(k, i, l+1)] - self.U[(k, i, l)])
+        
+        uk_xj = {}
+        for k in range(self.K):
+            for j in range(self.P):
+                uk_xj[k, j] = quicksum(uik_xij[k, i, j] for i in range(self.n))
+        
+        uk_yj = {}
+        for k in range(self.K):
+            for j in range(self.P):
+                uk_yj[k, j] = quicksum(uik_yij[k, i, j] for i in range(self.n))
+
+        # Binary variables for the preference of X over Y 
+        self.delta = {
+            (k, j): self.model.addVar(
+                vtype=GRB.BINARY, name="delta_{}_{}".format(k, j))
+                for k in range(self.K)
+                for j in range(self.P)
+        }
+
+        # Overestimation  and underestimation of the utilities
+        self.sigmaxplus = {
+            (j): self.model.addVar(
+                vtype=GRB.CONTINUOUS, lb=0, name="sigmaxplus_{}".format(j), ub=1)
+                for j in range(self.P)
+        }
+        self.sigmaxminus = {
+            (j): self.model.addVar(
+                vtype=GRB.CONTINUOUS, lb=0, name="sigmaxminus_{}".format(j), ub=1)
+                for j in range(self.P)
+        }
+        self.sigmayplus = {
+            (j): self.model.addVar(
+                vtype=GRB.CONTINUOUS, lb=0, name="sigmayplus_{}".format(j), ub=1)
+                for j in range(self.P)
+        }
+        self.sigmayminus = {
+            (j): self.model.addVar(
+                vtype=GRB.CONTINUOUS, lb=0, name="sigmayminus_{}".format(j), ub=1)
+                for j in range(self.P)
+        }
+
+        # Constraints
+        # The preference of X over Y with the binary variables
+        M = 100
+
+        self.model.addConstrs(
+            (uk_xj[k, j] - self.sigmaxp[j] + self.sigmaxm[j] - uk_yj[k, j] + self.sigmayp[j] - self.sigmaym[j] - self.epsilon >= -M*(1-self.delta1[(k,j)]) for j in range(self.P) for k in range(self.K))
+        )
+        # There's at least one cluster that prefers X over Y
+        for j in range(self.P):
+            self.model.addConstr(
+                quicksum(self.delta1[(k, j)] for k in range(self.K)) >= 1
+            )
+        # The function must be ascending
+        self.model.addConstrs(
+            (self.U[(k, i, l+1)] - self.U[(k, i, l)]>=self.epsilon for k in range(self.K) for i in range(self.n) for l in range(self.L)))
+
+        # Utility starting point
+        self.model.addConstrs(
+            (self.U[(k, i, 0)] == 0 for k in range(self.K) for i in range(self.n)))
+        # Utility function normalization
+        self.model.addConstrs(
+            (quicksum(self.U[(k, i, self.L)] for i in range(self.n)) == 1 for k in range(self.K)))
+        
+        # Objective of our model
+        self.model.setObjective(quicksum(self.sigmaxp[j] + self.sigmaxm[j] + self.sigmayp[j] + self.sigmaym[j] for j in range(self.P)), GRB.MINIMIZE)
+
+        # Solving our problem
+        self.model.params.outputflag = 0  # Mute mode for Gurobi
+        self.model.update()
+        self.model.optimize()
+        if self.model.status == GRB.INFEASIBLE:
+            print("\n Unfeasible")
+            raise Exception("Unfeasible")
+        elif self.model.status == GRB.UNBOUNDED:
+            print("\n Unbounded")
+            raise Exception("Unbounded")
+        else:
+            print("\n Solution found")
+            # print the value of objective function
+            print("objective function value: ", self.model.objVal)
+            self.U = {(k, i, l): self.U[k, i, l].x for k in range(self.K) for i in range(self.n) for l in range(self.L+1)}
+            self.delta1 = {(k, j): self.delta1[k, j].x for k in range(self.K) for j in range(self.P)}
+
+            sampleX = np.expand_dims(X[0], axis=0)
+            sampleY = np.expand_dims(Y[0], axis=0)
+            utilityX = self.predict_utility(sampleX)
+            utilityY = self.predict_utility(sampleY)
+            print("utility X: ", utilityX)
+            print("utility Y: ", utilityY) 
+        return self
 
     def predict_utility(self, X):
         """Return Decision Function of the MIP for X. - To be completed.
